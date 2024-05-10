aa02bdaa2c568e632bb4fe4641ac01e2bb8a5eb2
RuthLopesDiniz <ruthma1964@hotmail.com>
2024-05-09 22:23:07 -0300

    Resolution exercisies

---
+++ Trilha_Java_Dio/src/ResultadoEscolar.java
@@ -0,0 +1,19 @@
+
+public class ResultadoEscolar {
+
+	public static void main(String[] args) {
+		int nota = 2;
+		
+		if(nota >=7) 
+			System.out.println("aprovado");
+		
+		else if (nota >= 5 && nota < 7)
+			System.out.println("Prova recuperação");
+			
+		else
+			System.out.println("Reprovado");
+		
+
+	}
+
+}
